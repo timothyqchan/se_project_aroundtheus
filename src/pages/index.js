3f1663773030df30e2e078199de0333d4096a2cd
Timothy Chan <timothyqchan@gmail.com>
2023-11-10 10:05:36 -0500

    added project-9 code

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,6 +22,7 @@ import Api from "../components/Api.js";
 // I have really been struggling with this project and I dont know what I'm doing wrong
 // I keep getting an error in Popup.js, but I don't know whats wrong with it
 // Thank you for your patience
+//
 
 // Initial start-up
 const { apiToken, apiUrl } = apiInfo;
