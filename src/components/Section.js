9a92dcd0b70bdfa99c841bcc0e76f4cf97e80707
Timothy Chan <timothyqchan@gmail.com>
2023-11-17 18:39:12 -0500

    site fix

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,13 +1,13 @@
 export default class Section {
   constructor({ items, renderer }, selector) {
     this._items = items;
-    this._renderer = renderer;
+    this.renderer = renderer;
     this._selector = document.querySelector(selector);
   }
 
   renderItems() {
     this._items.forEach((item) => {
-      this._renderer(item);
+      this.renderer(item);
     });
   }
 
