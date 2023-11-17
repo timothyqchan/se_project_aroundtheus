b3214bf840690382876cd1854fbd49633c44d4a6
Timothy Chan <timothyqchan@gmail.com>
2023-11-17 17:25:16 -0500

    project-9 final

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,11 +18,6 @@ import PopupWithConfirmation from "../components/PopupWithConfirmation.js";
 import UserInfo from "../components/UserInfo.js";
 import Api from "../components/Api.js";
 
-// Message to Tripleten reviewer: I'm so sorry for the terrible code.
-// I have really been struggling with this project and I dont know what I'm doing wrong
-// I keep getting an error in Popup.js, but I don't know whats wrong with it
-// Thank you for your patience
-
 // Initial start-up
 const { apiToken, apiUrl } = apiInfo;
 
